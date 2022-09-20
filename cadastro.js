8e4b1c437e69a11195431e7c39c847b733aa7daa
FerLuan <105259317+FerLuan@users.noreply.github.com>
2022-09-20 17:36:09 -0400

    Meu projeto de tela de cadastro

---
+++ cadastro.js
@@ -0,0 +1,39 @@
+//NOME
+function seuNomeFocus(){
+  document.querySelector('#seuNome').style.backgroundColor = "rgb(102, 31, 170)"
+}
+
+function seuNomeSemFocus(){
+  document.querySelector('#seuNome').style.backgroundColor = "blueviolet"
+}
+//NOME
+
+//EMAIL
+function seuLoginFocus(){
+  document.querySelector('#seuEmail').style.backgroundColor = "rgb(102, 31, 170)"
+}
+
+function seuLoginSemFocus(){
+  document.querySelector('#seuEmail').style.backgroundColor = "blueviolet"
+}
+//EMAIL
+
+//SENHA
+function suaSenhaFocus(){
+  document.querySelector('#suaSenha').style.backgroundColor = "rgb(102, 31, 170)"
+}
+
+function suaSenhaSemFocus(){
+  document.querySelector('#suaSenha').style.backgroundColor = "blueviolet"
+}
+//SENHA
+
+//CONFIRMAR SENHA
+function confirmeSenhaFocus(){
+  document.querySelector('#confirmeSenha').style.backgroundColor = "rgb(102, 31, 170)"
+}
+
+function confirmeSenhaSemFocus(){
+  document.querySelector('#confirmeSenha').style.backgroundColor = "blueviolet"
+}
+//CONFIRMAR SENHA
